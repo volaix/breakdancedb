698c0bddae39904e04133b98082c6b65fa4cc9b9
Mark David Teo <volaix@gmail.com>
2024-05-03 20:57:49 +0800

    increase placeholder for completed page

--- src/app/completed/page.tsx
+++ src/app/completed/page.tsx
@@ -1,4 +1,20 @@
+import Header from '@/app/Header' 
+const Completed = () => {
+ //get flows from localstorage
+ return (
+  <div className='mt-20 bg-white dark:bg-slate-800	'>
+  <h1>Flows</h1>
+  <div>test</div>
+  <h1>Transitions</h1>
+  <h1>Summary</h1>
+  <h1>Combos</h1>
+ </div>
+ )
+}
 const Page = () => {
- return <>hello world</>
+ return <div>
+  <Header/>
+  <Completed/>
+ </div>
 }
 export default Page
