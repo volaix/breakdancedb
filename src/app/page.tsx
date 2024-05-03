31582fe296e9d950483ca6273a974a340b240b09
Mark David Teo <volaix@gmail.com>
2024-05-03 19:11:10 +0800

    fixed button add some todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,8 +67,10 @@ const Home = () => {
   }
 
   const onClickYes = () => {
+    //TODO: FUTURE have a celebration UI element
     saveToLocalStorage()
     setLearningToRandom()
+    //TODO: FUTURE have refresh UI feeling
   }
   const onClickSkip = () => {
     setLearningToRandom()
@@ -76,6 +78,7 @@ const Home = () => {
   const onClickNo = () => {
     setLearningToRandom()
     //TODO: Have multiselect appear on where user could not complete. If none are selected then never show the flow combination again.
+    //TODO: FUTURE have refresh UI feeling
   }
 
   return (
@@ -94,21 +97,20 @@ const Home = () => {
           )}
         </div>
         <div className="py-5 px-2 flex justify-evenly">
-          {/* TODO: clicking buttons feels weird */}
           <a
             onClick={onClickYes}
-            className="px-6 py-2  text-center text-white bg-violet-600 border border-violet-600 rounded active:text-violet-500 hover:bg-transparent hover:text-violet-600 focus:outline-none focus:ring">
+            className="px-6 py-2 text-center text-white bg-violet-600 border border-violet-600 rounded ">
             Yes
           </a>
 
           <a
             onClick={onClickSkip}
-            className="px-6 py-2  text-center text-violet-600 border border-violet-600 rounded hover:bg-violet-600 hover:text-white active:bg-indigo-500 focus:outline-none focus:ring">
+            className="px-6 py-2 text-center text-violet-600 border border-violet-600 rounded">
             Skip
           </a>
           <a
             onClick={onClickNo}
-            className="px-6 py-2  text-center text-white bg-violet-600 border border-violet-600 rounded active:text-violet-500 hover:bg-transparent hover:text-violet-600 focus:outline-none focus:ring">
+            className="px-6 py-2 text-center text-white bg-violet-600 border border-violet-600 rounded">
             No
           </a>
         </div>
