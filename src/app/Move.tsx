2de30e6fb3fb7f84ce19a47a84abcb49f5db53c8
Mark David Teo <volaix@gmail.com>
2024-05-03 13:50:51 +0800

    updated with new move format

--- src/app/Move.tsx
+++ src/app/Move.tsx
@@ -1,12 +1,16 @@
 'use client'
 //TODO: add move type so it wont crash on production lol
-const Move = ({move}) => {
+const Move = ({move}: {move: string}) => {
   return (
     <>
       {move && (
         <div className="bg-slate-300 w-full py-3 flex flex-col items-center">
-          <img className="w-full w-5/6" alt="move name" src={move.cover_img} />
-          <div className="capitalize">{move.move_name}</div>
+          <img
+            className="w-full w-5/6"
+            alt="move name"
+            src={'https://dummyimage.com/600x400/000/fff'}
+          />
+          <div className="capitalize">{move}</div>
         </div>
       )}
     </>
