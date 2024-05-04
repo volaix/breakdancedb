1efdde5fdb8b9c33f4952093d0efe70267c9e8b2
Mark David Teo <volaix@gmail.com>
2024-05-04 12:04:00 +0800

    boilerplate + notes

---
+++ src/app/learnmoves/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/app/Header'
+const Page = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+       moves learning
+      <div className="mt-10">1. Example video. Screenshots. Upload example video. Upload self screenshots to compare. Each SS = move % learned</div>
+     </div>
+    </div>
+  )
+}
+export default Page
