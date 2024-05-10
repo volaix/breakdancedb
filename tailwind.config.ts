21c27578fb890646eeb1bdbea9f8374f84113011
Mark David Teo <volaix@gmail.com>
2024-05-10 11:46:55 +0200

    iphone se helper

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        'se': '375px'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
