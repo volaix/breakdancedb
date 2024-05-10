5f9a8299ce9dcdf0a365ff65734040780924824e
Mark David Teo <volaix@gmail.com>
2024-05-10 17:33:36 +0200

    put move into one file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 // @format
 import Header from "./Header"
-import Move from "./Move"
 import { useState, useEffect } from "react"
 import {
   Flow,
@@ -11,6 +10,7 @@ import {
   updateLocalStorageGlobal,
   useLocalStorage,
 } from "./lib"
+import Image from "next/image"
 
 
 //------------------------local utils------------------------------
@@ -19,7 +19,35 @@ const getRandomItem = (items: string[]) =>
 
 //------------------------localtypes-------------------------------
 type Learning = Flow | null
+//------------------------components-------------------------------
+/**
+ * Renders an Image with some text below for each one of the flows
+ * @param 
+ * @returns 
+ */
+const Move = ({ move }: { move: string }) => {
+  return (
+    <>
+      {move && (
+        <div className="bg-slate-300 dark:bg-gray-900 w-full py-3 flex flex-col items-center">
+          <Image
+            width="600"
+            height="400"
+            className="w-full w-5/6"
+            alt="move name"
+            src={'https://dummyimage.com/600x400/000/fff'}
+          />
+          <div className="capitalize text-black dark:text-white">{move}</div>
+        </div>
+      )}
+    </>
+  )
+}
 
+//----------------------------mainrender--------------------------
+/*
+ * Renders 3 moves with 3 buttons at the bottom.
+ */
 export default function Home() {
   //-----------------------------state-----------------------------
   const [accessToLocalStorage, setAccessToLocalStorage] = useState(false)
