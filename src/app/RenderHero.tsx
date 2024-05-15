60cc62fa37c1cdc9cf74bb00a1b031e767b85331
Mark David Teo <volaix@gmail.com>
2024-05-15 17:18:38 +0200

    fixed buttons

--- src/app/RenderHero.tsx
+++ src/app/RenderHero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function RenderHero() {
     return (<section className="text-gray-600 body-font">
@@ -6,19 +7,27 @@ export default function RenderHero() {
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 {/* TODO make title component. fixes light dark additions all the time*/}
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                
-                BreakdanceDB
+
+                    BreakdanceDB
                 </h1>
                 <br className="hidden lg:inline-block" />
                 <p className="mb-8 leading-relaxed"> Ditch the scrapbook.  Record your footworks, practice systematically, flow in the cypher.  Remember sets.  Welcome to your new breakdance database for footwork.
                 </p>
                 <div className="flex justify-center">
-                    <button className="text-xs inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                        Add Footworks
-                    </button>
-                    <button className="text-xs ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                        RNG Flows
-                    </button>
+                    <Link
+                        href={{ pathname: '/learnmoves' }}
+                    >
+                        <button className="text-xs inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            Add Footworks
+                        </button>
+                    </Link>
+                    <Link
+                        href={{ pathname: '/learnflows' }}
+                    >
+                        <button className="text-xs ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                            RNG Flows
+                        </button>
+                    </Link>
                 </div>
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
