6c00ad418e6ebd0f656e9217dc115a4978f29876
Mark David Teo <volaix@gmail.com>
2024-06-03 12:22:30 +0800

    FEATURE: sort button

--- src/app/yourmoves/page.tsx
+++ src/app/yourmoves/page.tsx
@@ -21,26 +21,19 @@ import { isValidKey } from '../_utils/lsValidation'
 import { useZustandStore } from '../_utils/zustandLocalStorage'
 
 //---------------------------utils---------------------------------
-const makeArray = (moveString: string): string[] => {
-  return moveString.split('\n')
-}
-const makeString = (moveArray: string[]): string => {
-  return moveArray.join('\r\n')
-}
+const makeArray = (moveString: string): string[] =>
+  moveString.split('\n').map((str) => str.trim())
+
+const makeString = (moveArray: string[]): string => moveArray.join('\r\n')
 
 const hasDuplicates = (array: string[]): boolean => {
   const uniqueItems = new Set(array)
   return uniqueItems.size !== array.length
 }
 
-//---------------------------types---------------------------------
-
-type Inputs = {
-  categoryMoves: string
-}
-
-//----------------------components-----------------------------
-
+/**
+ * gets mapped as categories user can select
+ */
 const categories: {
   label: string
   key: keyof GlobalStateProperties[typeof lsUserMoves]
@@ -55,6 +48,15 @@ const categories: {
   { label: 'Blow Ups', key: lsBlowups },
   { label: 'Misc', key: lsMisc },
 ]
+
+//---------------------------types---------------------------------
+
+type Inputs = {
+  categoryMoves: string
+}
+
+//----------------------components-----------------------------
+
 /**
  * Component that allows user to put in their moves into the database
  * one line per move in a text file.
@@ -129,6 +131,12 @@ const RenderYourMoves = () => {
     }
   }, [lsMoves, unsavedMoveList])
 
+  //----------------functions----------
+  const sortMovesInTextArea = () => {
+    setLsMoves(makeString(makeArray(unsavedMoveList).sort()))
+    setSaveButtonActive(true)
+  }
+
   //---------------------------handlers-----------------------------
 
   const handleChangeCategory = (
@@ -195,7 +203,8 @@ const RenderYourMoves = () => {
                 message: 'ERROR: DUPLICATE MOVES.',
                 visible: true,
               })
-              setLsMoves(makeString(movesArr.sort()))
+              sortMovesInTextArea()
+              // setLsMoves(makeString(movesArr.sort()))
               return
             } else if (isValidKey(selectedKey)) {
               setLsUserMovesByKey(selectedKey, movesArr)
@@ -205,53 +214,66 @@ const RenderYourMoves = () => {
               setNotification({ message: 'Saved', visible: true })
             }
           })}
-          className="-m-2 flex flex-wrap"
+          className="relative -m-2 flex w-full flex-wrap p-2"
         >
           {/* --------------------text area form--------------------------- */}
-          <section className="relative w-full p-2">
-            {/* ---------------loaded text------------------------ */}
-            <label className="absolute ml-1 mt-5 text-[9px]">
-              {`${hasLoaded ? selectedKey + ' loaded' : 'Not loaded'}`}
-            </label>
-            <h2 className="text-sm capitalize leading-7 text-gray-600 dark:text-gray-400">
-              {`Your ${selectedKey ? selectedKey + 's' : 'moves'}`}
-            </h2>
-            {/* ---------------text area------------------------ */}
-            <section className="flex max-w-xs space-x-4 p-4">
-              {/* -----------------left textarea------------------- */}
-              <textarea
-                {...register('categoryMoves')}
-                className="h-32 w-8/12 max-w-fit resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-xs text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 dark:border-gray-700 dark:bg-gray-800 dark:bg-opacity-40 dark:text-gray-100 dark:focus:bg-gray-900 dark:focus:ring-indigo-900"
-              />
-              {/* --------------------right json view-------------------- */}
-              <pre className="h-32 w-4/12 max-w-28 overflow-y-auto rounded-lg bg-gray-100 p-4 text-[10px] text-xs">
-                {JSON.stringify(unsavedMoveList.split(/\r\n|\r|\n/), null, 1)}
-              </pre>
-            </section>
-            <p className="text-xs">
-              {`calculated ${unsavedMoveList.split(/\r\n|\r|\n/).length} moves from above. New move created each line. Saved is ${lsMoves.split(/\r\n|\r|\n/).length} moves.`}
-            </p>
-            <Notification
-              visible={!!notification?.visible}
-              message={notification?.message || ''}
+          {/* ---------------loaded text------------------------ */}
+          <label className="absolute ml-1 mt-5 text-[9px]">
+            {`${hasLoaded ? selectedKey + ' loaded' : 'Not loaded'}`}
+          </label>
+          <h2 className="text-sm capitalize leading-7 text-gray-600 dark:text-gray-400">
+            {`Your ${selectedKey ? selectedKey + 's' : 'moves'}`}
+          </h2>
+          {/* ---------------text area------------------------ */}
+          <section className="flex max-w-xs space-x-4 p-4">
+            {/* -----------------left textarea------------------- */}
+            <textarea
+              {...register('categoryMoves')}
+              className="h-32 w-8/12 max-w-fit resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-xs text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 dark:border-gray-700 dark:bg-gray-800 dark:bg-opacity-40 dark:text-gray-100 dark:focus:bg-gray-900 dark:focus:ring-indigo-900"
             />
+            {/* --------------------right json view-------------------- */}
+            <pre className="h-32 w-4/12 max-w-28 overflow-y-auto rounded-lg bg-gray-100 p-4 text-[10px] text-xs">
+              {JSON.stringify(unsavedMoveList.split(/\r\n|\r|\n/), null, 1)}
+            </pre>
+          </section>
+          <p className="text-xs">
+            {`above ${unsavedMoveList.split(/\r\n|\r|\n/).length} moves. Saved ${lsMoves.split(/\r\n|\r|\n/).length} moves. New move created each line. `}
+          </p>
+          <Notification
+            visible={!!notification?.visible}
+            message={notification?.message || ''}
+          />
+          {/* -------------Buttons-------------- */}
+          <section className="mt-5 flex w-full justify-center">
+            {/* ----------sort button------- */}
+            <button
+              className="flex items-center justify-center rounded border 
+              border-indigo-500 px-8 py-2 text-center  
+              text-indigo-500"
+              onClick={(e) => {
+                //prevents form submit
+                e.preventDefault()
+                sortMovesInTextArea()
+              }}
+            >
+              <label className="text-lg leading-none">Sort</label>
+            </button>
             {/* -------------Save Button------------------ */}
             <button
               disabled={!saveButtonActive}
               type="submit"
-              className="mx-auto mt-5 flex rounded border-0 bg-indigo-500 px-8 py-2 text-lg text-white hover:bg-indigo-600 focus:outline-none disabled:opacity-50"
+              className="flex rounded border-0 bg-indigo-500 
+              px-8 py-2 text-lg text-white hover:bg-indigo-600 
+              focus:outline-none disabled:opacity-50"
             >
               {saveText}
             </button>
-            <button
-              className="flex h-fit items-center justify-center rounded border border-indigo-500 px-3 py-2 text-center text-xs text-indigo-500"
-              onClick={(e) => {
-                e.preventDefault()
-              }}
-            >
-              <label className="text-xs leading-none">Sort list</label>
-            </button>
           </section>
+          <p className="text-[7px]">
+            sort button cannot be undone. Sorts current moves in alphabetical
+            order
+          </p>
+          {/* ---------end of buttons--------- */}
         </form>
         {/* ------------end of form----------- */}
       </article>
