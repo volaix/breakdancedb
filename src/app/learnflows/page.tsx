17d6c4d910956a4f50c6305c0490a41cae338beb
Mark David Teo <volaix@gmail.com>
2024-06-06 17:37:18 +0800

    adv option done

--- src/app/learnflows/page.tsx
+++ src/app/learnflows/page.tsx
@@ -89,6 +89,7 @@ export default function RenderFlows() {
   //learning refers to "what will be displayed" and is RNG set
   const [learning, setLearning] = useState<BasicFlow | null>(null)
   const [visible, setVisible] = useState(false)
+  const [hideUnique, setHideUniques] = useState(false)
   const [notificationMessage, setNotificationMessage] = useState<string>('')
   const [textAreaValue, setTextAreaValue] = useState<string>('')
   const [singleCategory, setSingleCategory] = useState<boolean>(true)
@@ -112,8 +113,11 @@ export default function RenderFlows() {
   const getLsUserMovesByKey = useZustandStore(
     (state) => state.getLsUserMovesByKey,
   )
+  const [moveTransitions, setMoveTransitions] = useState<MoveTransition[]>()
 
   //----------------functions----------------
+  const isSameMove = (move1: BasicMove, move2: BasicMove): boolean =>
+    move1.displayName === move2.displayName && move1.category === move2.category
 
   const shuffleLearning = useCallback(
     (single?: keyof BasicFlow) => {
@@ -138,6 +142,50 @@ export default function RenderFlows() {
   )
 
   //---------------------------hooks---------------------------------
+  type MoveTransition = {
+    moveFrom: BasicMove
+    moveTo: BasicMove
+  }
+
+  /**
+ * For ADV. OPT. Hide Uniques
+  Set move transitions
+ */
+  useEffect(() => {
+    if (!hideUnique) return
+    const lsFlows = getLsFlows()
+    if (!lsFlows) return
+    const moveTransitions: MoveTransition[] = Object.values(lsFlows).flatMap(
+      (flowVal): MoveTransition[] => {
+        if (!flowVal) return []
+        const { entryMove, keyMove, exitMove } = flowVal
+        const midMove: BasicMove = {
+          category: keyMove.category,
+          displayName: keyMove.displayName,
+        }
+        return [
+          {
+            moveFrom: {
+              category: entryMove.category,
+              displayName: entryMove.displayName,
+            },
+            moveTo: midMove,
+          },
+          {
+            moveFrom: midMove,
+            moveTo: {
+              category: exitMove.category,
+              displayName: exitMove.displayName,
+            },
+          },
+        ]
+      },
+    )
+
+    setMoveTransitions(moveTransitions)
+
+    // setMovesUsedInFlow(basicMoves)
+  }, [getLsFlows, hideMovesIfFlow, hideUnique])
 
   //if hideMovesIfFlow flag, set movesUsedInFlow
   useEffect(() => {
@@ -242,8 +290,7 @@ export default function RenderFlows() {
               <button
                 disabled={singleCategory}
                 onClick={() => setSingleCategory(true)}
-                className="px-4 py-1 focus:outline-none disabled:bg-indigo-500 
-  disabled:text-white dark:enabled:text-gray-300"
+                className="px-4 py-1 focus:outline-none disabled:bg-indigo-500 disabled:text-white dark:enabled:text-gray-300"
               >
                 Single
               </button>
@@ -272,12 +319,23 @@ export default function RenderFlows() {
                     />
                   </article>
                   <article>
-                    <label> Hide moves already used in Flows </label>
+                    <label>Hide moves already used in Flows</label>
                     <input
                       className="ml-1"
                       checked={hideMovesIfFlow}
                       onChange={(e) => {
                         setHideMovesIfFlow(e.target.checked)
+                      }}
+                      type="checkbox"
+                    />
+                  </article>
+                  <article>
+                    <label>Hide non unique transitions in saved flows</label>
+                    <input
+                      className="ml-1"
+                      checked={hideUnique}
+                      onChange={(e) => {
+                        setHideUniques(e.target.checked)
                       }}
                       type="checkbox"
                     />
@@ -292,10 +350,7 @@ export default function RenderFlows() {
             <div className="flex flex-col text-center">
               No moves to display.
               <Link href="/yourmoves">
-                <button
-                  className="bg-indigo-500 px-4 py-1 text-white
-  focus:outline-none dark:text-gray-300"
-                >
+                <button className="bg-indigo-500 px-4 py-1 text-white focus:outline-none dark:text-gray-300">
                   Add moves
                 </button>
               </Link>
@@ -308,8 +363,8 @@ export default function RenderFlows() {
                 ['entryMove', 'keyMove', 'exitMove'] as Array<
                   keyof SelectedCategoryState
                 >
-              ).map((movePosition, i) => (
-                <div key={i} className="relative flex">
+              ).map((movePosition, movePosIndex, movePosArr) => (
+                <div key={movePosIndex} className="relative flex">
                   {/* //-------------------------DROPDOWN------------------------- */}
                   <article className="w-1/2">
                     {/* title */}
@@ -317,12 +372,8 @@ export default function RenderFlows() {
                     {/* select */}
                     <div className="relative">
                       <select
-                        disabled={i !== 0 && singleCategory}
-                        className="focus:shadow-outline block w-full appearance-none rounded-lg border border-gray-300 bg-white px-4 py-2 pr-10
-  leading-tight
- focus:outline-none enabled:hover:border-gray-500 disabled:opacity-35 
- dark:border-indigo-500 dark:bg-transparent dark:bg-none
- dark:text-white dark:disabled:opacity-10"
+                        disabled={movePosIndex !== 0 && singleCategory}
+                        className="focus:shadow-outline block w-full appearance-none rounded-lg border border-gray-300 bg-white px-4 py-2 pr-10 leading-tight focus:outline-none enabled:hover:border-gray-500 disabled:opacity-35 dark:border-indigo-500 dark:bg-transparent dark:bg-none dark:text-white dark:disabled:opacity-10"
                         value={
                           singleCategory
                             ? selectedCategory['entryMove']
@@ -349,7 +400,7 @@ export default function RenderFlows() {
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg
-                          className={`h-4 w-4 fill-current dark:fill-indigo-500 ${singleCategory && i !== 0 && 'opacity-30'}`}
+                          className={`h-4 w-4 fill-current dark:fill-indigo-500 ${singleCategory && movePosIndex !== 0 && 'opacity-30'}`}
                           fill="#000000"
                           height="800px"
                           width="800px"
@@ -368,10 +419,9 @@ export default function RenderFlows() {
                     {displayMoves && (
                       <section className="h-full w-full dark:bg-gray-900 dark:text-white">
                         <label>{`${selectedCategory[movePosition]} move`}</label>
-                        <div className="relative flex w-full appearance-none items-center justify-between overflow-hidden rounded-lg border border-gray-300  dark:border-indigo-500">
+                        <div className="relative flex w-full appearance-none items-center justify-between overflow-hidden rounded-lg border border-gray-300 dark:border-indigo-500">
                           <select
-                            className="focus:shadow-outline block w-full appearance-none rounded-lg border border-none border-gray-300 bg-transparent py-2 pl-2 leading-tight 
-  focus:outline-none enabled:hover:border-gray-500 disabled:opacity-35"
+                            className="focus:shadow-outline block w-full appearance-none rounded-lg border border-none border-gray-300 bg-transparent py-2 pl-2 leading-tight focus:outline-none enabled:hover:border-gray-500 disabled:opacity-35"
                             value={learning[movePosition]}
                             onChange={(e) => {
                               setLearning((prev) => ({
@@ -385,30 +435,66 @@ export default function RenderFlows() {
                             {getLsUserMovesByKey(selectedCategory[movePosition])
                               .toSorted()
                               .map((moveStr) => {
-                                //should hide based on advanced flags
-                                const shouldHideMove = (
-                                  hideCondition: boolean,
-                                  movesUsed?: BasicMove[],
-                                ) =>
-                                  hideCondition &&
-                                  movesUsed?.some(
-                                    ({ displayName, category }) =>
-                                      displayName === moveStr &&
-                                      category ===
-                                        selectedCategory[movePosition],
-                                  )
+                                //---------ADV. OPT. FUNCTION -----------
+                                const advOptionIsTicked =
+                                  hideUnique ||
+                                  hideMovesIfFlow ||
+                                  hideMovesIfBattle
 
-                                if (
-                                  shouldHideMove(
-                                    hideMovesIfBattle,
-                                    movesUsedInBattle,
-                                  ) ||
-                                  shouldHideMove(
-                                    hideMovesIfFlow,
-                                    movesUsedInFlow,
-                                  )
-                                ) {
-                                  return
+                                if (advOptionIsTicked) {
+                                  // Don't show option if this move and past move match an existing transition
+                                  if (hideUnique) {
+                                    const thisMove: BasicMove = {
+                                      category: selectedCategory[movePosition],
+                                      displayName: moveStr,
+                                    }
+                                    const pastMove: BasicMove = {
+                                      category:
+                                        selectedCategory[
+                                          movePosArr[movePosIndex - 1]
+                                        ],
+                                      displayName:
+                                        learning[movePosArr[movePosIndex - 1]],
+                                    }
+                                    if (
+                                      moveTransitions?.some(
+                                        ({ moveFrom, moveTo }) =>
+                                          isSameMove(moveTo, thisMove) &&
+                                          isSameMove(moveFrom, pastMove),
+                                      )
+                                    ) {
+                                      return
+                                    }
+                                  }
+
+                                  const areMovesAlreadyUsed = (
+                                    hideCondition: boolean,
+                                    movesUsed?: BasicMove[],
+                                  ): boolean => {
+                                    return (
+                                      hideCondition &&
+                                      !!movesUsed?.some(
+                                        ({ displayName, category }) =>
+                                          displayName === moveStr &&
+                                          category ===
+                                            selectedCategory[movePosition],
+                                      )
+                                    )
+                                  }
+
+                                  // Don't show option if moves are already used
+                                  if (
+                                    areMovesAlreadyUsed(
+                                      hideMovesIfBattle,
+                                      movesUsedInBattle,
+                                    ) ||
+                                    areMovesAlreadyUsed(
+                                      hideMovesIfFlow,
+                                      movesUsedInFlow,
+                                    )
+                                  ) {
+                                    return
+                                  }
                                 }
 
                                 return (
