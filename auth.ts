9cab8f2b4466ad551c24fdda5dafc6669aaf7182
Mark David Teo <volaix@gmail.com>
2024-06-18 13:13:32 +0800

    gets userid and removes twitter github

--- auth.ts
+++ auth.ts
@@ -4,6 +4,25 @@ import google from 'next-auth/providers/google'
 import Twitter from 'next-auth/providers/twitter'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Twitter, google],
+  providers: [
+    GitHub,
+    // Twitter,
+    // google
+  ],
   basePath: '/auth',
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        // User is available during sign-in
+        token.id = user.id
+      }
+      return token
+    },
+    session({ session, token }) {
+      if (typeof token.id === 'string') {
+        session.user.id = token.id
+      }
+      return session
+    },
+  },
 })
