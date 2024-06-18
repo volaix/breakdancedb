32480bf0d68c87559dec9eb1bc4a0e352a716d5d
Mark David Teo <volaix@gmail.com>
2024-06-18 13:13:44 +0800

    boilerplate

--- src/app/_utils/actions.ts
+++ src/app/_utils/actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { signIn, signOut } from '../../../auth'
+import { auth, signIn, signOut } from 'auth'
 
 export const signInAction = async () => {
   console.log('trying to sign in')
@@ -10,4 +10,23 @@ export const signInAction = async () => {
 export const signOutAction = async () => {
   console.log('trying to sign out')
   await signOut()
+}
+
+export const saveUser = async (userId: string, localStorage: string) => {
+  const session = await auth()
+  try {
+    const response = await fetch('https://api.example.com/data')
+    if (!response.ok) {
+      throw new Error('Failed to fetch data')
+    }
+
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const loadUser = async () => {
+  console.log('loadUser')
 }
