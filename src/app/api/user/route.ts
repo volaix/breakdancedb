84cfbc16dfd1c04e9bae5061cf0f6ceb23218654
Mark David Teo <volaix@gmail.com>
2024-06-22 09:19:45 +0800

    added lastEdited

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,17 @@
 'use server'
-import { NextUser } from '@/app/RenderButtonTest'
+import { NextUser } from '@/app/RenderCloudButtons'
 import dbClientPromise from '@/db/mongodb'
 import { auth } from 'auth'
+import { User } from 'next-auth'
 import { NextRequest, NextResponse } from 'next/server'
 
 const getUserCollection = async () => {
   return await dbClientPromise.db('BreakdanceDB').collection('userData')
+}
+
+export const getUserData = async (): Promise<User | null> => {
+  const session = await auth()
+  return session?.user ?? null
 }
 
 export async function GET() {
@@ -13,9 +19,12 @@ export async function GET() {
   try {
     const user = await (
       await getUserCollection()
-    ).findOne({ user: session?.user }, { projection: { _id: 0, userDb: 1 } })
+    ).findOne(
+      { user: session?.user },
+      { projection: { _id: 0, userDb: 1, editedAt: 1 } },
+    )
 
-    return NextResponse.json({ userDb: user?.userDb }, { status: 200 })
+    return NextResponse.json({ ...user }, { status: 200 })
   } catch (e) {
     console.error(e)
   }
@@ -30,6 +39,7 @@ export async function PUT(req: NextRequest, res: NextResponse) {
     $set: {
       user: session?.user,
       userDb: { ...data },
+      editedAt: new Date(Date.now()),
     },
   }
   const options = { upsert: true }
