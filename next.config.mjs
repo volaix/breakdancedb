c87d84ba6adbe9c7a14d53cbf88509b07a39ce82
Mark David Teo <volaix@gmail.com>
2024-06-22 23:34:52 +0800

    hotfix

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5090/api/:path*', // HOTFIX
+      },
+    ]
+  },
 }
 
 export default nextConfig
