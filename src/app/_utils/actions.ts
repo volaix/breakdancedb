50c67ebc6c4977ef474e03905b7c92aad318cbfa
Mark David Teo <volaix@gmail.com>
2024-06-24 11:32:36 +0800

    remove unused code

--- src/app/_utils/actions.ts
+++ src/app/_utils/actions.ts
@@ -1,28 +1,13 @@
 'use server'
 
-import { auth, signIn, signOut } from 'auth'
+import { signIn, signOut } from 'auth'
 
 export const signInAction = async () => {
+  console.log('signinaction, signing in')
   await signIn()
 }
 
 export const signOutAction = async () => {
   console.log('trying to sign out')
   await signOut()
-}
-
-export const saveUser = async (userId: string, localStorage: {}) => {
-  console.log('running saveuser')
-  const session = await auth()
-  try {
-    const response = await fetch('https://api.example.com/data')
-    if (!response.ok) {
-      throw new Error('Failed to fetch data')
-    }
-
-    const data = await response.json()
-    console.log(data)
-  } catch (error) {
-    console.error(error)
-  }
 }
