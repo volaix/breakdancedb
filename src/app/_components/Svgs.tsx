7ba56cc3cb22222950760f3ede2c4d1286808e66
Mark David Teo <volaix@gmail.com>
2024-06-27 23:20:01 +0800

    added svgs and removed unused code

--- src/app/_components/Svgs.tsx
+++ src/app/_components/Svgs.tsx
@@ -116,8 +116,6 @@ export const RenderUpArrow = ({ ...props }: SVGProps<SVGSVGElement>) => (
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       fill="#000000"
-      height="800px"
-      width="800px"
       version="1.1"
       id="Layer_1"
       viewBox="0 0 330 330"
