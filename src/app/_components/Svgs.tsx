7494030047530684d76195720da845e4c6d880bf
Mark David Teo <volaix@gmail.com>
2024-07-01 23:43:28 +1000

    better descriptions

--- src/app/_components/Svgs.tsx
+++ src/app/_components/Svgs.tsx
@@ -93,7 +93,6 @@ export const RenderRedHoldButton = ({ ...props }: SVGProps<SVGSVGElement>) => {
 export const RenderGreyTick = ({ ...props }: SVGProps<SVGSVGElement>) => (
   <>
     <svg
-      {...props}
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
@@ -101,6 +100,7 @@ export const RenderGreyTick = ({ ...props }: SVGProps<SVGSVGElement>) => (
       strokeWidth="2.5"
       className="h-3 w-3"
       viewBox="0 0 24 24"
+      {...props}
     >
       <path d="M20 6L9 17l-5-5" />
     </svg>
@@ -187,29 +187,27 @@ export const RenderUpArrow = ({ ...props }: SVGProps<SVGSVGElement>) => (
  * @returns jsx
  */
 export const RenderDownArrow = ({ ...props }: SVGProps<SVGSVGElement>) => (
-  <>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      fill="#000000"
-      height="800px"
-      width="800px"
-      version="1.1"
-      id="Layer_1"
-      viewBox="0 0 330 330"
-      xmlSpace="preserve"
-      {...props}
-    >
-      <path
-        id="XMLID_225_"
-        d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393  c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393  s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
-      />
-    </svg>
-  </>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    fill="#000000"
+    height="800px"
+    width="800px"
+    version="1.1"
+    id="Layer_1"
+    viewBox="0 0 330 330"
+    xmlSpace="preserve"
+    {...props}
+  >
+    <path
+      id="XMLID_225_"
+      d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393  c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393  s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+    />
+  </svg>
 )
 
 /**
- * renders a grey tick
+ * renders a
  * @returns jsx
  */
 export const RenderRedoIcon = ({ ...props }: SVGProps<SVGSVGElement>) => (
