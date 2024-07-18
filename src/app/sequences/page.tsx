a3c234b65fbb2e7a18d455f300211966fc2e7bc7
Mark David Teo <volaix@gmail.com>
2024-07-18 11:31:18 +0800

    adds new seq

--- src/app/sequences/page.tsx
+++ src/app/sequences/page.tsx
@@ -120,6 +120,7 @@ export default function RenderViewCombos() {
         <button
           onClick={() => {
             addRound()
+            updateSequences()
           }}
           className="mt-5 inline-flex rounded border-0 bg-indigo-500 px-6 py-2 text-xs text-white hover:bg-indigo-600 focus:outline-none"
         >
